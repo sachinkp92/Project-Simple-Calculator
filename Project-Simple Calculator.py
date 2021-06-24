3cb0a613194e126219729a1919b24f82b092242e
Sachin Kumar <83978025+sachinkp92@users.noreply.github.com>
2021-06-24 16:45:13 +0530

    Add files via upload

---
+++ Project-Simple Calculator.py
@@ -0,0 +1,14 @@
+#Calculator
+num1 = float(input("Enter a first number:"))
+op = input("Enter operator:")
+num2 = float(input("Enter a second number:"))
+if op == "+":
+    print(num1+num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "*":
+    print(num1*num2)
+else:
+    print("Invalid operator")
